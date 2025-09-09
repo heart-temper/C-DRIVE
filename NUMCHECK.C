2b2e801efa5e37d1ef1ee9dc7e4c24ffe1c16fee
heart-temper <anirrudhuniyal.au@gmail.com>
2025-09-09 14:14:27 +0530

    Add files via upload

---
+++ NUMCHECK.C
@@ -0,0 +1,7 @@
+#include <stdio.h>
+void main(){
+	int input;
+	scanf("%d",&input);
+	if(input>0) printf("num is positive");
+	else if(input==0) printf("num is 0");
+	else printf("num is negative");}

1226a47c3abd5af1958ae9281a97971809fae6ab
heart-temper <anirrudhuniyal.au@gmail.com>
2025-09-10 13:33:24 +0530

    Delete GREATERN.C

--- GREATERN.C
+++
@@ -1,9 +0,0 @@
-//finding the largest of two numbers
-#include <stdio.h>
-void main(){
-	int num1, num2;
-	printf("enter two numbers to be checked for which is greater or smaller");
-	scanf("%d%d",&num1,&num2);
-	if(num1>num2) printf("%d is greater than %d",num1,num2);
-	else if(num2>num1) printf("%d is greater than %d",num2,num1);
-	else printf("invalid comparision");}

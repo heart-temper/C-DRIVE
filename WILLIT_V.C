012c228481aede89874994175ccc6e4b5f3e2136
heart-temper <anirrudhuniyal.au@gmail.com>
2025-09-11 10:48:10 +0530

    Add files via upload
    ternary matryoshka :D

---
+++ WILLIT_V.C
@@ -0,0 +1,11 @@
+/* making letter checker, this is the second try with ternary operator
+-11|09|2025*/
+#include <stdio.h>
+#define will_it_vow main
+void will_it_vow(){
+	char vowo;
+	printf("\n hi");
+	scanf(" %c",&vowo);
+	printf("%c \n",vowo);
+	(vowo=='a')?printf("%c is vowel",vowo):(vowo=='e')?printf("%c is vowel",vowo):(vowo=='i')?printf("%c is vowel",vowo):(vowo=='o')?printf("%c is vowel",vowo):(vowo=='u')?printf("%c is vowel",vowo):printf("%c is consonant :P",vowo);
+	getch();}

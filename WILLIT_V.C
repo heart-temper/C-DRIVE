7da5523dd1b3e749899eb6a1d613169ab49d40a5
heart-temper <anirrudhuniyal.au@gmail.com>
2025-09-11 10:50:11 +0530

    Update WILLIT_V.C
    removed the getch and added a doubt

--- WILLIT_V.C
+++ WILLIT_V.C
@@ -1,4 +1,5 @@
 /* making letter checker, this is the second try with ternary operator
+can i make this more efficient without using data structures?
 -11|09|2025*/
 #include <stdio.h>
 #define will_it_vow main
@@ -7,5 +8,5 @@ void will_it_vow(){
 	printf("\n hi");
 	scanf(" %c",&vowo);
 	printf("%c \n",vowo);
-	(vowo=='a')?printf("%c is vowel",vowo):(vowo=='e')?printf("%c is vowel",vowo):(vowo=='i')?printf("%c is vowel",vowo):(vowo=='o')?printf("%c is vowel",vowo):(vowo=='u')?printf("%c is vowel",vowo):printf("%c is consonant :P",vowo);
-	getch();}
+	(vowo=='a')?printf("%c is vowel",vowo):(vowo=='e')?printf("%c is vowel",vowo):(vowo=='i')?printf("%c is vowel",vowo):(vowo=='o')?printf("%c is vowel",vowo):(vowo=='u')?printf("%c is vowel",vowo):printf("%c is consonant :P",vowo);}
+

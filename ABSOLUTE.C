5e72d60d584701cf7c7736e21be6a9d7ad4e9c9e
heart-temper <anirrudhuniyal.au@gmail.com>
2025-09-16 10:49:30 +0530

    Add files via upload

---
+++ ABSOLUTE.C
@@ -0,0 +1,9 @@
+/*still sad :(
+-16|09|2025
+RAHHHHHHHHHHHHHHH*/
+#include <stdio.h>
+void main (){ int RAHHHHH;
+	printf("gimme ");
+	scanf("%d",&RAHHHHH);
+	if(RAHHHHH<0) RAHHHHH= -1*RAHHHHH;
+	printf("%d ",RAHHHHH);}
